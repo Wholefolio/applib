a12e17aa806379561e2fa72688ecce2d27d1c473
Atanas Kozhuharov <atanas.kozhuharov@sumup.com>
2021-06-15 14:41:43 +0300

    Proper URL to applib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='applib',
       version='1.3.3',
       description='App library - common toolbox library for applications',
-      url='http://gitlab.com/wholefolio/applib',
+      url='https://github.com/wholefolio/applib',
       author='Atanas K',
       author_email='support@wholefolio.io',
       license='MIT',
